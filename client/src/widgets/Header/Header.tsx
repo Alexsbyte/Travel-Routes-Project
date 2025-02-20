67001823f125967a3b97afb640bb7184bd3fd594
Shabanov Mikhail <sensdoo.dev@gmail.com>
2025-02-20 16:16:35 +0300

    added routes, header, layout, edited tsconfig

---
+++ client/src/widgets/Header/Header.tsx
@@ -0,0 +1,67 @@
+import { Box, Burger, Button, Drawer, Group, ScrollArea } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import classes from './header.module.css';
+import React from 'react';
+
+export function Header(): React.JSX.Element {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+
+  return (
+    <Box pb={120}>
+      <header className={classes.header}>
+        <Group justify="space-between" h="100%">
+          <MantineLogo size={30} />
+
+          <Group visibleFrom="sm">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+
+          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+        </Group>
+      </header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        hiddenFrom="sm"
+        zIndex={1000000}
+      >
+        <ScrollArea h="calc(100vh - 80px" mx="-md">
+          {/* <Divider my="sm" /> */}
+
+          {/* <a href="#" className={classes.link}>
+            Home
+          </a>
+          <UnstyledButton className={classes.link} onClick={toggleLinks}>
+            <Center inline>
+              <Box component="span" mr={5}>
+                Features
+              </Box>
+              <IconChevronDown size={16} color={theme.colors.blue[6]} />
+            </Center>
+          </UnstyledButton>
+          <Collapse in={linksOpened}>{links}</Collapse>
+          <a href="#" className={classes.link}>
+            Learn
+          </a>
+          <a href="#" className={classes.link}>
+            Academy
+          </a> */}
+
+          {/* <Divider my="sm" /> */}
+
+          <Group justify="center" grow pb="xl" px="md">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </Box>
+  );
+}
