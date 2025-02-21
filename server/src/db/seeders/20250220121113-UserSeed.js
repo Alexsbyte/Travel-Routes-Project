0ff79165724cf8ad139cd8fc3f508792856c2dd8
VoroninVladimirN93 <vva8khv@gmail.com>
2025-02-21 12:07:58 +0300

    addRoutes model and migrations +user

--- server/src/db/seeders/20250220121113-UserSeed.js
+++ server/src/db/seeders/20250220121113-UserSeed.js
@@ -1,9 +1,12 @@
 "use strict";
 const bcrypt = require('bcrypt');
+require('dotenv').config();
+console.log(`${process.env.ADMIN_PASSWORD}`);
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const hashedPasswordAdmin = await bcrypt.hash("Qwerty123!", 10);
+    const hashedPasswordAdmin = await bcrypt.hash(`${process.env.ADMIN_PASSWORD}`, 10);
     await queryInterface.bulkInsert(
       "Users",
       [
