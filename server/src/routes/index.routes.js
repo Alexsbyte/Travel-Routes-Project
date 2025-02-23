c826f1d821f6f8a77cb8087ce7dc6782db8e9784
Alexsbyte <kemper33157@gmail.com>
2025-02-23 16:56:26 +0300

    add methods

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const authRoutes = require('./user.routes');
 const routerRoutes = require('./router.routes');
 const uploadRouter = require('./api/uploads.routes');
+const pointRouter = require('./point.routes')
 
 const formatResponse = require('../utils/formatResponse');
 
 router.use('/auth', authRoutes);
 router.use('/uploads', uploadRouter);
 router.use('/routes', routerRoutes);
+router.use('/points' , pointRouter)
 
 router.use('*', (req, res) => {
   res.status(404).json(formatResponse(404, 'Not found', null, 'Resource not found'));
