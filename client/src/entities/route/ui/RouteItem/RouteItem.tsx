c21b477184f0e4881adace0e89dcd71eb9a12f9d
Shabanov Mikhail <sensdoo.dev@gmail.com>
2025-02-24 20:13:58 +0300

    added links to developers

--- client/src/entities/route/ui/RouteItem/RouteItem.tsx
+++ client/src/entities/route/ui/RouteItem/RouteItem.tsx
@@ -3,6 +3,8 @@ import { Carousel } from 'antd';
 import { Image, Tag } from 'antd';
 import styles from './RouteItem.module.css';
 import { Route } from '../../model';
+import { Link } from 'react-router-dom';
+import { CLIENT_ROUTES } from '@/shared/enums/client_routes';
 
 type Props = {
   route: Route;
@@ -17,7 +19,6 @@ const images = [
 ];
 
 export function RouteItem({ route }: Props): React.JSX.Element {
-
   const slides = images.map((url) => (
     <div key={url}>
       <Image preview={false} src={url} alt={`Route image ${url}`} />
@@ -59,7 +60,9 @@ export function RouteItem({ route }: Props): React.JSX.Element {
       {/* Описание */}
       <div className={styles.separator}></div>
       <div className={styles.description}>
-        <h3 className={styles.title}>{route.title}</h3>
+        <Link to={`${CLIENT_ROUTES.ROUTE_PAGE}/${route.id}`}>
+          <h3 className={styles.title}>{route.title}</h3>
+        </Link>
         <div className={styles.separator}></div>
         <p className={styles.text}>{route.description}</p>
       </div>
--- client/src/widgets/Footer/Footer.tsx
+++ client/src/widgets/Footer/Footer.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { Box, Group, rgba, Text } from '@mantine/core';
+import { Box, Flex, rgba, Text } from '@mantine/core';
+import { AiFillGithub } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 export function Footer(): React.JSX.Element {
   const currentYear = new Date().getFullYear();
   return (
-    <Box component="footer" py="md" mt={30} bg={rgba('gray', 0.1)}>
-      <Group h={75} justify="center">
+    <Box bg={rgba('gray', 0.05)} component="footer" py="md" mt={30}>
+      <Flex h={150} justify="space-around" align="center" px="md">
+        <Flex direction="row" gap="xs">
+          <Flex align="center" gap="xs">
+            <AiFillGithub size={20} />
+            <Link to="https://github.com/aselya93">
+              <Text>aselya93</Text>
+            </Link>
+          </Flex>
+          <Flex align="center" gap="xs">
+            <AiFillGithub size={20} />
+            <Link to="https://github.com/Alexsbyte">
+              <Text>Alexsbyte</Text>
+            </Link>
+          </Flex>
+          <Flex align="center" gap="xs">
+            <AiFillGithub size={20} />
+            <Link to="https://github.com/VoroninVladimirN93">
+              <Text>VoroninVladimirN93</Text>
+            </Link>
+          </Flex>
+          <Flex align="center" gap="xs">
+            <AiFillGithub size={20} />
+            <Link to="https://github.com/EkaterinaMkh">
+              <Text>EkaterinaMkh</Text>
+            </Link>
+          </Flex>
+          <Flex align="center" gap="xs">
+            <AiFillGithub size={20} />
+            <Link to="https://github.com/sensdoo-dev">
+              <Text>sensdoo-dev</Text>
+            </Link>
+          </Flex>
+        </Flex>
         <Text>Buffaloes team &copy; {currentYear}</Text>
-      </Group>
+      </Flex>
     </Box>
   );
 }
